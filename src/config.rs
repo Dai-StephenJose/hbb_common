dd68e47ed46bd7c4c8e1157272c86396f725b3a7
Dai-StephenJose <d670677773@gmail.com>
2025-06-06 23:27:51 +0800

    change server info
    change server info

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["opu1.daish.eu.org"];
+pub const RS_PUB_KEY: &str = "Sr1xylxkw11AhHU6KmaU2nbHx68VFm+Z7B6NFBKVNxs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
